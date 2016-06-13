4494d2c7ad78fd9dc7c6d6fdd8e5ae391bf611f4
jordanhrzg <jordan.a.herzog@asu.edu>
2016-06-12 21:16:18 -0500

    Update TileListener.java
    Code clean up and commenting

--- TileListener.java
+++ TileListener.java
@@ -1,27 +1,23 @@
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing.*;
 
 /**
  * Created by Jordan on 6/9/2016.
  */
 public class TileListener implements ActionListener {
-
     public void actionPerformed(ActionEvent e) {
-
-//        ImageIcon red = createImageIcon("resources/oceanpic.jpg");
-//        ImageIcon grey = createImageIcon("resources/oceanpic.jpg");
-
+        
+        //test prints
         System.out.println("The " + ((Tile) e.getSource()).getTileID() + " button is clicked");
         System.out.println("Button is occupied: " + ((Tile) e.getSource()).isOccupied());
 
         Tile tile = ((Tile) e.getSource());
-
+        
+        //change tile color: red hit, blue miss
         if (tile.isOccupied()) {
             ((Tile) e.getSource()).setBackground(Color.RED);
         } else {
             ((Tile) e.getSource()).setBackground(Color.BLUE);
         }
     }
-
 }
