44b2078e7245cebeab66a9223ef89339a5f667bf
jordanhrzg <jordan.a.herzog@asu.edu>
2016-06-12 21:03:58 -0500

    Update Fleet.java
    Code clean up and commenting

--- Fleet.java
+++ Fleet.java
@@ -2,14 +2,16 @@ import java.util.ArrayList;
 
 /**
  * Created by Jordan on 6/12/2016.
+ * 
+ * Move to Ship or Gameboard Class?
  */
+
 public class Fleet {
-
-    ArrayList fleet = new ArrayList<Ship>();
-
+    
     final int FLEET_SIZE = 5;
-
-
+    ArrayList fleet = new ArrayList<Ship>();
+    
+//-----------------------------------------------------------Create Standard Fleet    
     public Fleet(){
 
         fleet.add(new Ship(5));
@@ -17,13 +19,14 @@ public class Fleet {
         fleet.add(new Ship(3));
         fleet.add(new Ship(3));
         fleet.add(new Ship(2));
-
     }
-
+    
+//---------------------------------------------------------------Get Ship in Fleet 
     public Ship getShip(int index){
-        return ((Ship)fleet.get(index));
-    }
-
+    return ((Ship)fleet.get(index));
+}
+    
+//--------------------------------------------------------------------------------
     public int getFleetSize(){
         return FLEET_SIZE;
     }
